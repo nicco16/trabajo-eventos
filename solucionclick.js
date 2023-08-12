a9b6fd56251a752bcb4b46c2f60ef0e4fafd5b50
nicco16 <ncamiletti4@gmail.com>
2023-08-12 10:30:21 -0300

    listo

--- solucionclick.js
+++ solucionclick.js
@@ -1,19 +1,15 @@
-
 document.addEventListener('DOMContentLoaded', function(){
-    const divElement = document.getElementById('miDiv');
-    const buttonElement = document.getElementById('miBoton');
+const divElement = document.getElementById('miDiv');
+const buttonElement = document.getElementById('miBoton');
+
+buttonElement.addEventListener('click', (event) => {
+    // Evitar la propagación del evento clic del botón al div
+    event.stopPropagation();
+
     
-    buttonElement.addEventListener('click', (event) => {
-        // Evitar la propagación del evento clic del botón al div
-        event.stopPropagation();
+});
+
+divElement.addEventListener('click', () => {
     
-        // Ejecutar la acción del botón
-      
-    });
-    
-    divElement.addEventListener('click', () => {
-        // Esto solo se ejecutará si haces clic en el div, no en el botón
-       
-    });  
-    });
-    
+}); 
+});   
